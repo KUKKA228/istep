5ae9d01d6c484582f79b026fc163bdeca7557252
kruzo <bycenkoaeksandr08@gmail.com>
2022-12-16 16:48:18 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Good afternoon Tatiana Vladimirovna!")
+#Класс Компьютер
+class Computer:
+    def __init__(self, model, *args, **kwargs):
+        #Метод super()
+        super().__init__(*args, **kwargs)
+        #Переменная (Модель,Память)
+        self.model = model
+        self.memory = 128
+#Класс Дисплей
+class Display:
+    def init (self, *args, **kwargs):
+        #Метод super()
+        super().__init__(*args, **kwargs)
+        #Переменная (Разрешение)
+        self.resolution = "4k"
+#Класс Смартфон
+class SmartPhone(Display, Computer):
+    def print_info(self):
+        print(self.model)
+        print(self.resolution)
+        print(self.memory)
+iphone = SmartPhone(model = "Last")
+iphone.print_info()
+
