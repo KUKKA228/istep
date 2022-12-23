67287f4a222e10a3e9eb3dfcbec6b809025a031c
kruzo <bycenkoaeksandr08@gmail.com>
2022-12-23 10:14:15 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-#Класс Компьютер
-class Computer:
-    def __init__(self, model, *args, **kwargs):
-        #Метод super()
-        super().__init__(*args, **kwargs)
-        #Переменная (Модель,Память)
-        self.model = model
-        self.memory = 128
-#Класс Дисплей
-class Display:
-    def init (self, *args, **kwargs):
-        #Метод super()
-        super().__init__(*args, **kwargs)
-        #Переменная (Разрешение)
-        self.resolution = "4k"
-#Класс Смартфон
-class SmartPhone(Display, Computer):
-    def print_info(self):
-        print(self.model)
-        print(self.resolution)
-        print(self.memory)
-iphone = SmartPhone(model = "Last")
-iphone.print_info()
+import colorama
+help(colorama)
+help(colorama.ansi)
+# Functions-чітка лінія,чистий екран,код до символів,встановлення назви
+# CLASSES
+#     builtins.object
+#         AnsiCodes
+#             AnsiBack
+#             AnsiFore
+#             AnsiStyle
+#         AnsiCursor
 
