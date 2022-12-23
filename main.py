1b5cc380dc0278e8c88b748a8b9bec9eb9c86c37
kruzo <bycenkoaeksandr08@gmail.com>
2022-12-23 11:51:04 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import colorama
-help(colorama)
-help(colorama.ansi)
-# Functions-чітка лінія,чистий екран,код до символів,встановлення назви
-# CLASSES
-#     builtins.object
-#         AnsiCodes
-#             AnsiBack
-#             AnsiFore
-#             AnsiStyle
-#         AnsiCursor
-
+result = []
+def divider(a, b):
+ if a < b:
+    raise ValueError
+ if b > 100:
+    raise IndexError
+ return a / b
+data = {10: 2, 2: 5, 123: 4, 18: 0, 9: 15, 8: 4}
+try:
+ for key in data:
+  res = divider(key, data[key])
+except:
+ result.append(res)
+print(result)
